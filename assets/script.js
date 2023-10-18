420bd1a8b992cd04bb9596fc5d4f83a88c07c61e
Laurie-anne Doré <lau.adore@gmail.com>
2023-10-18 10:05:17 -0400

    ajout des flèches

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,30 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const arrowLeft = document.getElementsByClassName("arrow_left");
+const arrowRight = document.getElementsByClassName("arrow_right");
+
+arrowLeft.eventListener("click", function () {
+	if (currentSlide === 0) {
+		currentSlide = slides.length - 1;
+	} else {
+		currentSlide--;
+	}
+	showSlide();
+})
