f6544448f855bce85b1fd3e245b88029e77a091f
Laurie-anne Doré <lau.adore@gmail.com>
2023-10-20 09:29:12 -0400

    Carousel update : dots

--- assets/script.js
+++ assets/script.js
@@ -27,26 +27,46 @@ const banner = document.querySelector('.banner-img');
 const tagLine = document.querySelector('.banner-tagline');
 const imgDisplay = './assets/images/slideshow/';
 
+const dots = document.querySelector(".dots");
+const selectDot = document.getElementsByClassName("dot");
+
+
 
 // ******************* VARIABLES *******************
 
 let currentSlide = 0;
 // Slide.length = 4;
 
+for (const object of slides) {
+  const addDot = document.createElement("span");
+  addDot.classList = "dot";
+  dots.appendChild(addDot);
+}
+
 
 // ******************* FUNCTIONS *******************
 
-// Carousel update : images and tagline
+// Carousel update : img and tag
 function slideRight() {
   currentSlide = (currentSlide + 1 ) % slides.length;
   banner.setAttribute('src', imgDisplay + slides[currentSlide].image);
   tagLine.innerHTML = slides[currentSlide].tagLine;
+  updateDots();
 }
 
 function slideLeft() {
   currentSlide = (currentSlide + slides.length - 1) % slides.length;
   banner.setAttribute('src', imgDisplay + slides[currentSlide].image);
   tagLine.innerHTML = slides[currentSlide].tagLine;
+  updateDots();
+}
+
+// Carousel update : dots
+function updateDots() {
+  for (let i = 0; i < selectDot.length; i++) {
+    selectDot[i].classList.remove("dot_selected");
+  }
+    selectDot[currentSlide].classList.add("dot_selected");
 }
 
 
